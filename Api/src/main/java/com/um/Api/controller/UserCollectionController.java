424e90090b0dc1143eeee5cbf8e6077bbf54f89a
sindhuvyshaki <sindhuvyshaki@gmail.com>
2018-10-25 16:12:45 -0700

    Updated pom to run spring boot maven plugin only against API module (API is the application module). Cleaned up warnings.

--- Api/src/main/java/com/um/Api/controller/UserCollectionController.java
+++ Api/src/main/java/com/um/Api/controller/UserCollectionController.java
@@ -2,14 +2,10 @@ package com.um.Api.controller;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
